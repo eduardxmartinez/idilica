88aa30e94b18c2cfd4ccdd197daa82e7c50a4f33
eduardxmartinez <eduardom17809@gmail.com>
2022-03-02 04:49:34 -0700

    change modal display

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 const navClose = document.querySelector(".header-menu-close");
 const navOpen = document.querySelector(".header-menu-open");
 const modal = document.querySelector(".modal-menu");
+const body = document.querySelector("body");
 
 navClose.addEventListener("click", closeMenu);
 navOpen.addEventListener("click", openMenu);
 
 function openMenu() {
+  body.style.overflowY = "hidden";
   navOpen.style.display = "none";
   navClose.style.display = "block";
-  modal.style.zIndex = "1";
+  modal.style.zIndex = "2";
 }
 function closeMenu() {
   navClose.style.display = "none";
