46219b2e2a0805e07ef4b9898a8d8bde4b5159dd
eduardxmartinez <eduardom17809@gmail.com>
2022-03-04 03:31:13 -0700

    modal animation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //modal
 const navClose = document.querySelector(".header-menu-close");
 const navOpen = document.querySelector(".header-menu-open");
-const modal = document.querySelector(".nav");
+const modal = document.querySelector(".modal-menu");
 const body = document.querySelector("body");
 
 navClose.addEventListener("click", closeMenu);
@@ -11,11 +11,13 @@ function openMenu() {
   body.style.overflowY = "hidden";
   navOpen.style.display = "none";
   navClose.style.display = "block";
-  modal.style.display = "flex";
+  modal.style.transition = "500ms";
+  modal.style.transform = "translateY(1000px)";
 }
 function closeMenu() {
   body.style.overflowY = "scroll";
   navClose.style.display = "none";
   navOpen.style.display = "block";
-  modal.style.display = "none";
+  modal.style.transition = "500ms";
+  modal.style.transform = "translateY(-1000px)";
 }
