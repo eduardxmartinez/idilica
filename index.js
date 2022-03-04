5f029717ce18e50d999f9df6d49c1aeb43cf6552
eduardxmartinez <eduardom17809@gmail.com>
2022-03-03 19:42:36 -0700

    colecciones create

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //modal
 const navClose = document.querySelector(".header-menu-close");
 const navOpen = document.querySelector(".header-menu-open");
-const modal = document.querySelector(".modal-menu");
+const modal = document.querySelector(".nav");
 const body = document.querySelector("body");
 
 navClose.addEventListener("click", closeMenu);
@@ -11,11 +11,11 @@ function openMenu() {
   body.style.overflowY = "hidden";
   navOpen.style.display = "none";
   navClose.style.display = "block";
-  modal.style.zIndex = "1";
+  modal.style.display = "flex";
 }
 function closeMenu() {
   body.style.overflowY = "scroll";
   navClose.style.display = "none";
   navOpen.style.display = "block";
-  modal.style.zIndex = "0";
+  modal.style.display = "none";
 }
